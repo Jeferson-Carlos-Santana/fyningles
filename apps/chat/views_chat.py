f7d1912a609c860b208a0000338ba31ddc3bb453
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2025-12-22 20:18:19 +0000

    Teste envio de audio 14

--- apps/chat/views_chat.py
+++ apps/chat/views_chat.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from .models import LessonLine  # ajuste se o nome for outro
+from .models import LessonLine
 
 def chat(request, lesson_id):
-    line = (
+    lines = (
         LessonLine.objects
         .filter(lesson_id=lesson_id)
-        .order_by("id")   # ou "order"/"position"
-        .first()
+        .order_by("id")   # ou seq/position
     )
-
-    phrase_html = line.text if line else "<em>Sem frases</em>"
 
     return render(request, "chat/chat.html", {
         "lesson_id": lesson_id,
-        "phrase_html": phrase_html,
+        "lines": lines,
     })
