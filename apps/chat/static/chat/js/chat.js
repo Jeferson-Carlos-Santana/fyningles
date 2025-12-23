e9733a979b106eb881e7b451ac06fec8c19f7b96
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2025-12-23 18:01:28 +0000

    teste 47

--- apps/chat/static/chat/js/chat.js
+++ apps/chat/static/chat/js/chat.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // ===== SPEECH =====
-  const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
-  recognition.lang = "en-US";
-  recognition.continuous = false;
-  recognition.interimResults = false;
+
 
   // ===== ELEMENTOS =====
   const msgs = document.querySelectorAll(".chat-message");
