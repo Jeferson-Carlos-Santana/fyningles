4f19c95321eeb0b060777e3bae9533d165fe239e
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2026-01-02 11:57:31 +0000

    268

--- fyningles/urls.py
+++ fyningles/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.chat.urls")),
+    path("login/", LoginView.as_view(template_name="chat/login.html"), name="login"),
 ]
 
 
