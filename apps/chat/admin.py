442d3d0fd6956e6f5b27c01d2ce687029327998a
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2026-01-19 18:47:28 +0000

    530

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -123,7 +123,7 @@ class ChatAdmin(admin.ModelAdmin):
   
   ordering = ("lesson_id", "-seq")
   sortable_by = ()
-  list_per_page = 5000
+  list_per_page = 500
   
   # FUNCAO INFORMAL
   def gerar_informal(self, frase: str) -> str:
