f4f09dbef3861267c829e9ebc3a45fbb407b638c
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2026-01-26 15:50:31 +0000

    594

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -210,8 +210,7 @@ def phrase_progress(request):
         Progress.objects
         .filter(user=user)
         .select_related("chat")
-        #.order_by("-updated_at")[:100] 
-        .order_by("-points")[:100] # limita frases em andamento para usuario
+        .order_by("-points")[:1000] # limita frases em andamento para usuario
     )
 
     frases = []
