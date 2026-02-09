86ad082eadf28d1989f06538abfe08c2fe26aac3
Jeferson Carlos Santana <jefersoncarlossantana@gmail.com>
2026-02-09 19:22:31 +0000

    1002

--- apps/chat/static/chat/js/chat.js
+++ apps/chat/static/chat/js/chat.js
@@ -54,7 +54,7 @@ const USER_NAME = document.body.dataset.username || "";
       const recognition = new SpeechRecognition(); 
       window.recognition = recognition;
       recognition.lang = "en-GB";
-      recognition.continuous = true;
+      recognition.continuous = false;
       recognition.interimResults = true;
 
       const msgs = document.querySelectorAll(".chat-message");
